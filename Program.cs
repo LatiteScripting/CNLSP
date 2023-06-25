4f51a5aeb1f07cffd005fdbd8d0c74e04597ac5f
Plextora <71889427+Plextora@users.noreply.github.com>
2023-06-25 14:38:30 -0400

    ChakraCore check

--- Program.cs
+++ Program.cs
@@ -9,12 +9,42 @@
             Console.ResetColor();
         }
 
+        private static void DownloadFile(string url, string path)
+        {
+            using HttpClient client = new();
+            using Task<Stream> stream = client.GetStreamAsync(url);
+            using FileStream fileStream = new(path, FileMode.OpenOrCreate);
+            stream.Result.CopyTo(fileStream);
+            if (stream.IsCompletedSuccessfully)
+                WriteColor("Downloaded file successfully!", ConsoleColor.Green);
+        }
+
         private static readonly string LatiteFoler =
             $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\RoamingState\Latite";
 
+        private static readonly string UserName = Environment.UserName;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteColor($"Hello, {UserName}!\nThe program will setup your development environment shortly...",
+                ConsoleColor.White);
+
+            Thread.Sleep(3000);
+
+            WriteColor("\nChecking if ChakraCore.dll is in your Latite folder...", ConsoleColor.Yellow);
+
+            Thread.Sleep(1000);
+
+            if (File.Exists($"{LatiteFoler}\\ChakraCore.dll"))
+                WriteColor("ChakraCore.dll is in the Latite folder!", ConsoleColor.Green);
+            else
+            {
+                WriteColor("ChakraCore.dll is not in the Latite folder! Downloading ChakraCore.dll...",
+                    ConsoleColor.Red);
+                DownloadFile("https://latite-client.is-from.space/r/ChakraCore.dll", $"{LatiteFoler}\\ChakraCore.dll");
+            }
+
+            Thread.Sleep(1000);
         }
     }
 }
