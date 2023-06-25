1105b8caa1a52d5df3c3ee156da54d1488ca5959
Plextora <71889427+Plextora@users.noreply.github.com>
2023-06-25 13:33:22 -0400

    Setup some things

--- Program.cs
+++ Program.cs
@@ -2,9 +2,19 @@
 {
     internal class Program
     {
+        public static void WriteColor(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static readonly string LatiteFoler =
+            $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\RoamingState\Latite";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
